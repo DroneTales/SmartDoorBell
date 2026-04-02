825f69a91387d65bd4771a8386c9a28e31cd7a05
Mike Petrichenko <mike@btframework.com>
2026-04-02 15:51:13 +0300

    Combined with CamDoorbell

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Умный дверной звонок для Apple Home
 
-Этот репозиторий содерджит прошивку для устройства Apple HomeKit, о котором рассказывается в [этом видео](https://youtu.be/UexestUQARw).
+Этот репозиторий содерджит прошивку для устройства Apple HomeKit, о котором рассказывается в [этом](https://youtu.be/UexestUQARw) и [этом](https://youtu.be/UexestUQARw) видео.
  
 **Используемые компоненты**
 
@@ -9,8 +9,9 @@
 
 **Используемые библиотеки Arduino**
 
-- esp32 by Espressif Systems (board) 3.3.7
-- HomeSpan 2.1.7
+- esp32 by Espressif Systems (board)
+- HomeSpan
+- PubSubClient
  
 **Настройки Arduino IDE**
 
@@ -28,6 +29,99 @@
 - Zigbee Mode: Disabled
 - Programmer: Esptool
 
+## Конфигурация прокта
+
+Проект может быть сконфигурирован как для работы с видеокамерами через HomeBridge, так и как простой дверной звонок.  
+
+Если вы не используете видеокамеры в связке с HomeBridge, то найдите и закоментируйте строку
+
+`USE_HOMEBRIDGE_MQTT`  
+
+Если же вы используйете видеокамеры, то в следующем разделе вы найдете информацию о том, как правильно сконфигурировать HomeBridge для работы с дверным звонком.  
+
+По-умолчанию дверной звонок включает два переключателя: Звук и Уведомления. Первый управляет звуком дверного звонка, второй -- уведомлениями Apple Home.  
+
+Вы можете оставить только один переключатель. Для этого закоментируйте строку  
+
+`#define ENABLE_NOTIFICATIONS_SWITCH`  
+
+В этом случае останется только один выключатель, который будет полностью отключать и звук и уведомления.  
+
+## Установка MQTT брокера
+
+Этот раздел отписывает настройку HomeBridge для работы с дверным звонком с использованием камер. Если у вас нет камер или вы не используете HomeBridge, то этот раздел можно пропустить.  
+
+Подключитесь к вашей Raspberry Pi (или другому устройству, где у вас установлен HomeBridge) по SSH и введите следующуе команды:  
+
+`sudo apt-get update`  
+`sudo apt-get upgrade`  
+`sudo apt-get install mosquitto mosquitto-clients`  
+`sudo systemctl enable mosquitto`  
+`sudo nano /etc/mosquitto/mosquitto.conf`
+
+Последняя команда откроет файл конфигурации MQTT брокера. Удалите все строки и вставьте следующие:  
+
+```
+per_listener_settings true
+
+pid_file /run/mosquitto/mosquitto.pid
+
+persistence true
+persistence_location /var/lib/mosquitto/
+
+log_dest file /var/log/mosquitto/mosquitto.log
+
+include_dir /etc/mosquitto/conf.d
+
+listener 1883
+allow_anonymous false
+password_file /etc/mosquitto/passwd
+```
+
+Создайте нового пользователя для MQTT брокера. Для этого введите следующую команду:  
+
+`sudo mosquitto_passwd -c /etc/mosquitto/passwd dronetales`
+
+На запрос пароля введите `dronetales`.
+
+Запустите MQTT сервер (брокер), введя команду:  
+
+`sudo systemctl restart mosquitto`
+
+## Настройка HomeBridge
+
+Подключитесь к HomeBridge по Web интерфейсу. Выберите "Редактировать JSON". 
+
+В разделе CameraUI замените настройки MQTT следующими:
+
+```
+"mqtt": {
+    "active": true,
+    "tls": false,
+    "host": "127.0.0.1",
+    "port": 1883,
+    "username": "dronetales",
+    "password": "dronetales"
+},
+```
+
+Если в процессе создания MQTT пользователя вы использовали иные имя и пароль, то укажите их в параметрах "username" и "password".
+
+Пролистайте вниз до раздела "cameras" и найдите там раздел "mqtt". Измените его как показано ниже. Если в вашем файле конфигурации такого раздела нет, то добавьте его сразу после раздела "videoanalysis".  
+
+```
+"mqtt": {
+      "doorbellTopic": "doorcam/bell",
+      "doorbellMessage": "RING",
+},
+```
+
+Теперь добавьте следующую строку сразу перед секцией "videoConfig":
+
+`"doorbell": true,`
+
+Готово.
+
 ## Поддержать автора
  
 Если вам интересно то, что я делаю, вы можете поддержать меня используя ссылки ниже:  
@@ -38,4 +132,6 @@
 **BTC**: bitcoin:1A1WM3CJzdyEB1P9SzTbkzx38duJD6kau  
 **BCH**: bitcoincash:qre7s8cnkwx24xpzvvfmqzx6ex0ysmq5vuah42q6yz  
 **ETH**: 0xf780b3B7DbE2FC74b5F156cBBE51F67eDeAd8F9a  
+
+
 
--- SmartDoorBell.ino
+++ SmartDoorBell.ino
@@ -16,7 +16,34 @@
 //   - Programmer: Esptool
 // ===============================================================
 
+/**************************************************************************************/
+/*                                   Configuration                                    */
+
+// Comment this line if you want only one switch that disables doorbell sound and
+// notifications. If this line is uncommented the second switch will be added so you
+// can control doorbell notifications and sound separately.
+#define ENABLE_NOTIFICATIONS_SWITCH
+// Comment this line if you do not use the doorbell with HomeBridge cameras.
+#define USE_HOMEBRIDGE_MQTT
+
+/**************************************************************************************/
+
+
 #include <HomeSpan.h>
+#ifdef USE_HOMEBRIDGE_MQTT
+    #include <PubSubClient.h>
+    #include <WiFi.h>
+#endif
+
+
+// Do not change the lines below.
+    #ifndef ENABLE_NOTIFICATIONS_SWITCH
+        #define DISABLE_NOTIFICATIONS_SWITCH
+    #endif
+    #ifndef USE_HOMEBRIDGE_MQTT
+        #define USE_HOMEKIT_NOTIFICATION
+    #endif
+// Do not change the lines above.
 
 
 /**************************************************************************************/
@@ -47,15 +74,52 @@
 
 
 /**************************************************************************************/
+/*                                   MQTT  settings                                   */
+
+#ifdef USE_HOMEBRIDGE_MQTT
+    // MQTT server port.
+    const uint16_t MQTT_PORT = 1883;
+    // MQTT server address.
+    const char* const MQTT_SERVER = "192.168.1.13";
+    // MQTT client ID.
+    const char* const MQTT_CLIENT_ID = "DroneTales Doorbell";
+    // MQTT server user name.
+    const char* const MQTT_USER_NAME = "dronetales";
+    // MQTT server password.
+    const char* const MQTT_PASSWORD = "dronetales";
+    // Camera UI MQTT doorbell topic.
+    const char* const MQTT_DOORBELL_TOPIC = "doorcam/bell";
+    // Camera UI MQTT doorbell ring message.
+    const char* const MQTT_DOORBELL_MESSAGE = "RING";
+#endif
+
+/**************************************************************************************/
+
+
+/**************************************************************************************/
 /*                                  Global variables                                  */
 
-// The doorbell state. True if the doorbell is enabled and should play a sound.
-// False if the doorbell is disabled and should not play any sound.
-static bool BellEnabled = false;
-// Reference to the ProgrammableSwitchEvent Characteristic
-static SpanCharacteristic* BellEvent = nullptr;
 // Indicates when the ring button was pressed.
-static volatile bool BellRing = false;
+static volatile bool IsRinging = false;
+
+// The doorbell notifications state. True if the doorbell notifications are
+// enabled. False otherwise. By default the notifications is enabled.
+static bool NotificationsEnabled = true;
+// The doorbell state. True if the doorbell is enabled and should play a sound.
+// False if the doorbell is disabled and should not play any sound. By default
+// the doorbell sound is enabled.
+static bool DoorbellEnabled = true;
+
+#ifdef USE_HOMEBRIDGE_MQTT
+    // The WiFi client instalce.
+    static WiFiClient NetClient;
+    // The MQTT instance.
+    static PubSubClient MqttClient(NetClient);
+#endif
+#ifdef USE_HOMEKIT_NOTIFICATION
+    // The doorbell event object.
+    static SpanCharacteristic* DoorbellEvent = nullptr;
+#endif
 
 /**************************************************************************************/
 
@@ -63,38 +127,73 @@ static volatile bool BellRing = false;
 /**************************************************************************************/
 /*                                  Doorbell service                                  */
 
-struct Doorbell : Service::Doorbell
-{
-    Doorbell() : Service::Doorbell()
+#ifdef USE_HOMEKIT_NOTIFICATION
+    struct Doorbell : Service::Doorbell
     {
-        BellEvent = new Characteristic::ProgrammableSwitchEvent();
-    }
-};
+        Doorbell() : Service::Doorbell()
+        {
+            DoorbellEvent = new Characteristic::ProgrammableSwitchEvent();
+        }
+    };
+#endif
 
 /**************************************************************************************/
 
 
 /**************************************************************************************/
-/*                              Virtual  Doorbell switch                              */
+/*                             Virtual  Doorbell switches                             */
 
 struct DoorbellSwitch : Service::Switch
 {
-    SpanCharacteristic* Power;
+    SpanCharacteristic* Switch;
     
     DoorbellSwitch() : Service::Switch()
     {
         // Default is false (bell is turned off) and we store current value in NVS.
-        Power = new Characteristic::On(false, true);
-        // Get current state.
-        BellEnabled = Power->getVal();
+        Switch = new Characteristic::On(false, true);
+
+        // Get current states.
+        DoorbellEnabled = Switch->getVal();
+        #ifdef DISABLE_NOTIFICATIONS_SWITCH
+            // If the second (notifications) switch is disabled then use the
+            // same state for notifications as for doorbell sound.
+            NotificationsEnabled = DoorbellEnabled;
+        #endif
     }
     
     bool update()
     {
-        BellEnabled = Power->getNewVal();
+        DoorbellEnabled = Switch->getNewVal();
+        #ifdef DISABLE_NOTIFICATIONS_SWITCH
+            // If the second (notifications) switch is disabled then use the
+            // same state for notifications as for doorbell sound.
+            NotificationsEnabled = DoorbellEnabled;
+        #endif
+
         return true;
     }
 };
+
+#ifdef ENABLE_NOTIFICATIONS_SWITCH
+    struct NotificationSwitch : Service::Switch
+    {
+        SpanCharacteristic* Switch;
+    
+        NotificationSwitch() : Service::Switch()
+        {
+            // Default is false (bell is turned off) and we store current value in NVS.
+            Switch = new Characteristic::On(false, true);
+            // Get current state.
+            NotificationsEnabled = Switch->getVal();
+        }
+    
+        bool update()
+        {
+            NotificationsEnabled = Switch->getNewVal();
+            return true;
+        }
+    };
+#endif
 
 /**************************************************************************************/
 
@@ -107,21 +206,31 @@ void IRAM_ATTR RingInterrupt()
     static bool WasHigh = false;
     static uint32_t LastMillis = 0;
 
+    if (IsRinging)
+        return;
+
+    // Read the ring signal pin.
     uint32_t Level = digitalRead(BELL_BUTTON_PIN);
+    // If it is rased from LOW to HIGH...
     if (Level == HIGH && !WasHigh)
     {
+        // ...then set flag and store the time when the event appeared.
         WasHigh = true;
         LastMillis = millis();
+        // Exit from ISR as we need to wait when it downs from HIGH to LOW.
         return;
     }
 
+    // If the current level is LOW but was HIGH (FALING edge detected).
     if (Level == LOW && WasHigh)
     {
+        // Reset the previous state to normal (LOW).
         WasHigh = false;
 
-        uint32_t CurrentMillis = millis();
-        if (CurrentMillis - LastMillis >= BELL_BUTTON_SIGNAL_DURATION)
-            BellRing = true;
+        // Now calculate the pulse duration and if it looks like bell signal
+        // duration set the ringing flag.
+        uint32_t CurrentMillis = millis(); // We need this to be able to use unsigned values.
+        IsRinging = ((CurrentMillis - LastMillis >= BELL_BUTTON_SIGNAL_DURATION));
     }
 }
 
@@ -151,22 +260,34 @@ void setup()
     homeSpan.setControlPin(CONTROL_PIN);
     homeSpan.setStatusPin(STATUS_LED_PIN);
     homeSpan.setPairingCode("63005612");
-    homeSpan.begin(Category::Other, "DroneTales Doorbell");
-
+    homeSpan.begin(Category::Bridges, "DroneTales Doorbell Bridge");
+    
     // Build device's serial number.
     char Sn[24];
     snprintf(Sn, 24, "DRONETALES-%llX", ESP.getEfuseMac());
 
-    // Configure doorbell.
+    // Configure the Bridge accessory. We do not need to add name as it is taken
+    // from the begin() method.
     new SpanAccessory();
-    new Service::AccessoryInformation();
-    new Characteristic::Identify();
+	new Service::AccessoryInformation();
+	new Characteristic::Identify();
     new Characteristic::Manufacturer("DroneTales");
     new Characteristic::SerialNumber(Sn);
     new Characteristic::Model("DroneTales Doorbell");
-    new Characteristic::FirmwareRevision("1.0.0.0");
-    new Characteristic::Name("Doorbell");
-    new Doorbell();
+    new Characteristic::FirmwareRevision("1.0.1.0");
+    
+    #ifdef USE_HOMEKIT_NOTIFICATION
+        // Configure doorbell.
+        new SpanAccessory();
+        new Service::AccessoryInformation();
+        new Characteristic::Identify();
+        new Characteristic::Manufacturer("DroneTales");
+        new Characteristic::SerialNumber(Sn);
+        new Characteristic::Model("DroneTales Doorbell");
+        new Characteristic::FirmwareRevision("1.0.1.0");
+        new Characteristic::Name("Doorbell");
+        new Doorbell();
+    #endif
 
     // Configure doorbell switch.
     new SpanAccessory();
@@ -174,10 +295,29 @@ void setup()
     new Characteristic::Identify();
     new Characteristic::Manufacturer("DroneTales");
     new Characteristic::SerialNumber(Sn);
-    new Characteristic::Model("DroneTales Doorbell Switch");
-    new Characteristic::FirmwareRevision("1.0.0.0");
+    new Characteristic::Model("DroneTales Doorbell");
+    new Characteristic::FirmwareRevision("1.0.1.0");
+    new Characteristic::Name("Doorbell Sound Switch");
     new DoorbellSwitch();
 
+    #ifdef ENABLE_NOTIFICATIONS_SWITCH
+        // Configure notification switch.
+        new SpanAccessory();
+        new Service::AccessoryInformation();
+        new Characteristic::Identify();
+        new Characteristic::Manufacturer("DroneTales");
+        new Characteristic::SerialNumber(Sn);
+        new Characteristic::Model("DroneTales Doorbell");
+        new Characteristic::FirmwareRevision("1.0.1.0");
+        new Characteristic::Name("Doorbell Notification Switch");
+        new NotificationSwitch();
+    #endif
+
+    #ifdef USE_HOMEBRIDGE_MQTT
+        // Configure MQTT client.
+        MqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+    #endif
+    
     // Attached the interrupt to the ring signal pin.
     attachInterrupt(BELL_BUTTON_PIN, RingInterrupt, CHANGE);
 }
@@ -185,26 +325,58 @@ void setup()
 // Arduino main loop.
 void loop()
 {
-    bool DoRing = false;
+    // We need to copy the ring indicator. We need it because the IsRinging may change
+    // right in the middle of the routine. So we may not process notification but process
+    // only sound. So to make it working as expected we use the local copy.
+    bool RingDetected = IsRinging;
+    if (RingDetected)
+        // Reset it only if the ring was detected. Why? Because interrupt
+        // may happen right after flag copying and before the if statement.
+        IsRinging = false;
+    // If ring was detected (interrupt happened) after those statements then it will be
+    // processed on next iteration because the IsRinging flag will be set to True.
 
-    if (BellRing)
-    {
-        BellRing = false;
-
-        if (BellEnabled)
+    #ifdef USE_HOMEBRIDGE_MQTT
+        // Connect to MQTT server and send an MQTT message only if a ring was detected
+        // and notificatios are enabled. Of course, check the Wi-Fi network status too.
+        if (RingDetected && NotificationsEnabled && WiFi.status() == WL_CONNECTED)
         {
-            BellEvent->setVal(SpanButton::SINGLE);
-            DoRing = true;
+            // If the MQTT client is not connected then try to connect.
+            if (!MqttClient.connected())
+                MqttClient.connect(MQTT_CLIENT_ID, MQTT_USER_NAME, MQTT_PASSWORD);
+            
+            // Check the MQTT connection status as it may not connect by any reason.
+            if (MqttClient.connected())
+            {
+                // Now we can send a MQTT message about doorbell ring.
+                MqttClient.publish(MQTT_DOORBELL_TOPIC, MQTT_DOORBELL_MESSAGE);
+                // And disconnect from MQTT broker as we do not need to keep
+                // connection active for so long time (I am sure, no one rings the bell
+                // every single minute).
+                MqttClient.disconnect();
+            }
         }
-    }   
+    #endif
+    #ifdef USE_HOMEKIT_NOTIFICATION
+        // If we should send notification then just do it.
+        if (RingDetected && NotificationsEnabled)
+            DoorbellEvent->setVal(SpanButton::SINGLE);
+    #endif
 
+    // Now process the HomeSpan messages.
     homeSpan.poll();
 
-    if (DoRing)
+    // If bell is ringing.
+    if (RingDetected)
     {
-        digitalWrite(BELL_SIGNAL_PIN, HIGH);
-        delay(BELL_SIGNAL_DURATION);
-        digitalWrite(BELL_SIGNAL_PIN, LOW);
+        // And if the bell sound is enabled.
+        if (DoorbellEnabled)
+        {
+            // Play sound.
+            digitalWrite(BELL_SIGNAL_PIN, HIGH);
+            delay(BELL_SIGNAL_DURATION);
+            digitalWrite(BELL_SIGNAL_PIN, LOW);
+        }
     }
 }
 
